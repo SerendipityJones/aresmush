4dd8c3e5a96cffd8aa42e1ed32ff65aeb0027f5d
Ren <59793669+SerendipityJones@users.noreply.github.com>
2021-12-17 23:32:35 -0800

    Update portalscene_request_handler.rb

--- plugins/portalscene/web/portalscene_request_handler.rb
+++ plugins/portalscene/web/portalscene_request_handler.rb
@@ -8,19 +8,36 @@ module AresMUSH
 
         error = Website.check_login(request, true)
         return error if error
-        
-        locations = Global.read_config("portalscene", "location")
-        actions = Global.read_config("portalscene", "action")
-        things = Global.read_config("portalscene", "thing")
-        cues = Global.read_config("portalscene", "cue")
+
+        locations = Array.new
+        actions = Array.new
+        things = Array.new
+        cues = Array.new
+        ymlLocations = Global.read_config("portalscene", "location")
+        ymlLocations.each do |x|
+          locations << Website.format_markdown_for_html(x.to_s)
+        end
+        ymlActions = Global.read_config("portalscene", "action").each do |x|
+          actions << Website.format_markdown_for_html(x.to_s)
+        end
+        ymlThings = Global.read_config("portalscene", "thing").each do |x|
+          things << Website.format_markdown_for_html(x.to_s)
+        end
+        ymlCues = Global.read_config("portalscene", "cue").each do |x|
+          cues << Website.format_markdown_for_html(x.to_s)
+        end
 
         {
-          location: "#{locations.sample}",
-          action: "#{actions.sample}",
-          thing: "#{things.sample}",
-          cue: "#{cues.sample}"
+          location: locations,
+          action: actions,
+          thing: things,
+          cue: cues,
+          newLocation: "#{locations.sample}",
+          newAction: "#{actions.sample}",
+          newThing: "#{things.sample}",
+          newCue: "#{cues.sample}"
         }
-        
+
       end
     end
   end
