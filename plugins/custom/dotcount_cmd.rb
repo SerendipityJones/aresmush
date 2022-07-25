5d827afa6d702b205018fc687dbe0cb3e28fb173
Ren <59793669+SerendipityJones@users.noreply.github.com>
2022-07-25 06:47:57 -0700

    Update dotcount_cmd.rb

--- plugins/custom/dotcount_cmd.rb
+++ plugins/custom/dotcount_cmd.rb
@@ -16,8 +16,8 @@ module AresMUSH
         ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
           current_xp = model.xp
           pad_xp = current_xp < 10 ? '.' : ''
-          spent_attrs = FS3Skills::AbilityPointCounter.points_on_attrs(model)/2 + 12
-          spent_action = FS3Skills::AbilityPointCounter.points_on_action(model) + 18
+          spent_attrs = FS3Skills::AbilityPointCounter.points_on_attrs(model)/2 + Global.read_config('fs3skills', 'attributes').length * 2
+          spent_action = FS3Skills::AbilityPointCounter.points_on_action(model) + Global.read_config('fs3skills', 'action_skills').length
           model.fs3_attributes.each do |attr|
             if attr.rating == 1 then max_attrs-=1 end
           end
@@ -27,8 +27,8 @@ Total dots on #{model.name}'s sheet:
      Attributes...........#{spent_attrs} / #{max_attrs}
      Action Skills........#{spent_action} / #{max_action}
      Current XP...........#{pad_xp}#{current_xp.to_s} / #{max_xp}
-	 
-Any Attribute at 1 also lowers the Attribute cap by one. However, it will rise again if the Attribute is bought up to 2.	 
+
+Any Attribute at 1 also lowers the Attribute cap by one. However, it will rise again if the Attribute is bought up to 2.
 		  DOTS
           template = BorderedDisplayTemplate.new msg
           client.emit template.render
