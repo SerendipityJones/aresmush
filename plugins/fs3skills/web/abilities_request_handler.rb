83c5854eedc34fa6559e19e9d710f5a5213f2b49
Ren <59793669+SerendipityJones@users.noreply.github.com>
2022-08-01 07:55:25 -0700

    Formatting abilities page, adding 'n' for 'an' for roll

--- plugins/fs3skills/web/abilities_request_handler.rb
+++ plugins/fs3skills/web/abilities_request_handler.rb
@@ -11,25 +11,25 @@ module AresMUSH
           specialties: a['specialties'] ? a['specialties'].join(', ') : nil,
         }}
         languages = FS3Skills.languages.sort_by { |a| a['name'] }.map { |a| { name: a['name'], description: a['desc'] } }
-        advantages = FS3Skills.advantages.sort_by { |a| a['name'] }.map { |a| { name: a['name'], description: a['desc'] } }
-        
+        advantages = FS3Skills.advantages.sort_by { |a| a['name'] }.map { |a| {
+          name: FS3Skills.special_names.has_key?(a['name']) ? FS3Skills.special_names[a['name']] : a['name'], 
+          description: Website.format_markdown_for_html(a['desc']) } }
+
         {
           attrs_blurb: Website.format_markdown_for_html(FS3Skills.attr_blurb),
           action_blurb: Website.format_markdown_for_html(FS3Skills.action_blurb),
           background_blurb: Website.format_markdown_for_html(FS3Skills.bg_blurb),
           language_blurb: Website.format_markdown_for_html(FS3Skills.language_blurb),
-          advantages_blurb:  Website.format_markdown_for_html(FS3Skills.advantages_blurb),          
-          
+          advantages_blurb:  Website.format_markdown_for_html(FS3Skills.advantages_blurb),
+
           attrs: attrs,
           action_skills: action_skills,
           backgrounds: backgrounds,
           languages: languages,
           advantages: advantages,
           use_advantages: FS3Skills.use_advantages?
-        } 
+        }
       end
     end
   end
 end
-
-
